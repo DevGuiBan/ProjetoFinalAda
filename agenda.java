e1cea45d3d68ae1302d8bfe2b80eacb26958ca0d
Bowie <marketing@letscode.com.br>
2024-08-15 10:00:18 -0300

    agenda alterada para as solicitações do projeto correção de alguns imports que não estavam sendo utilizados, e faltando alguns passos da agenda ainda.

--- agenda.java
+++ agenda.java
@@ -1,67 +1,98 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class agenda {
     public static void main(String[] args) {
 
-        List<String> nomes = new ArrayList<>();
+        int[] agendaId = new int[5];
+        String[] agendaNome = new String[5];
+        String[] agendaTelefone = new String[5];
+        String[] agendaEmail = new String[5];
         Scanner input = new Scanner(System.in);
         int escolha = 1;
+        String telefone;
 
         while (escolha != 6) {
-            System.out.println("-- ESCOLHA --");
-            System.out.println("1 - Adicionar");
-            System.out.println("2 - Buscar");
-            System.out.println("3 - Atualizar");
-            System.out.println("4 - Deletar");
-            System.out.println("5 - Listar");
-            System.out.println("6 - Sair");
+            menuAgenda();
             escolha = input.nextInt();
             input.nextLine();
 
             switch (escolha) {
                 case 1:
-                    System.out.println("Informe o nome: ");
-                    String adicionar = input.nextLine();
-                    nomes.add(adicionar);
-                    System.out.println("O nome " + adicionar + " foi adicionado com sucesso!");
-                    break;
+                    System.out.println("Informe o telefone: ");
+                    telefone = input.nextLine();
+                    for (int i = 0; i < agendaTelefone.length; i++) {
+                        if (agendaTelefone[i].equals(telefone)) {
+                            System.out.println("O telefone já existe na agenda!");
+                            break;
+                        }else if (agendaTelefone[i].isEmpty()) {
+                            System.out.println("Informe o nome: ");
+                            String nome = input.nextLine();
+                            System.out.println("Informe o e-mail: ");
+                            String email = input.nextLine();
+                            agendaId[i] = i + 1;
+                            agendaTelefone[i] = telefone;
+                            agendaNome[i] = nome;
+                            agendaEmail[i] = email;
+                            System.out.println("Contato adicionado na agenda com sucesso!");
+                            detalharContato(agendaId[i], agendaNome[i], agendaTelefone[i], agendaEmail[i]);
+                            break;
+                        }
+                    }
                 case 2:
-                    System.out.println("Informe o nome que deseja buscar: ");
-                    String busca = input.nextLine();
-                    if (nomes.contains(busca)) {
-                        System.out.println("O nome " + busca + " está incluso na lista.");
-                    }else {
-                        System.out.println("Não existe o nome " + busca);
+                    System.out.println("Informe o telefone que deseja buscar: ");
+                    telefone = input.nextLine();
+                    for (int i = 0; i < agendaTelefone.length; i++) {
+                        if (agendaTelefone[i].equals(telefone)) {
+                            detalharContato(agendaId[i], agendaNome[i], agendaTelefone[i], agendaEmail[i]);
+                        }else {
+                            System.out.println("Não existe o contato buscado com o telefone: " + telefone);
+                        }
                     }
-                    break;
                 case 3:
-                    System.out.println("Informe o nome que deseja atualizar: ");
-                    String atualiza = input.nextLine();
-                    if (nomes.contains(atualiza)) {
-                        System.out.println("Informe o novo nome que deseja colocar: ");
-                        String novoNome = input.nextLine();
-                        int index = nomes.indexOf(atualiza);
-                        nomes.set(index, novoNome);
-                        System.out.println("O nome " + atualiza + " foi atualizado com sucesso para o nome " + novoNome);
-                    }else {
-                        System.out.println("O nome que deseja atualizar não está na lista!");
+                    System.out.println("Informe o telefone que deseja editar: ");
+                    telefone = input.nextLine();
+                    for (int i = 0; i < agendaTelefone.length; i++) {
+                        if (agendaTelefone[i].equals(telefone)) {
+                            detalharContato(agendaId[i], agendaNome[i], agendaTelefone[i], agendaEmail[i]);
+                            escolha = 0;
+                            while (escolha != 4) {
+                                menuEdicao();
+                                escolha = input.nextInt();
+                                input.nextLine();
+                                switch (escolha) {
+                                    case 1:
+                                        String nomeEditado = agendaNome[i];
+                                        System.out.println("Informe o nome que deseja cadastrar: ");
+                                        agendaNome[i] = input.nextLine();
+                                        System.out.println("O nome " + nomeEditado + " foi alterado para " + agendaNome[i] + " com sucesso!");
+                                        break;
+                                    case 2:
+                                        String telefoneEditado = agendaTelefone[i];
+                                        System.out.println("Informe o telefone que deseja cadastrar: ");
+                                        agendaTelefone[i] = input.nextLine();
+                                        System.out.println("O telefone " + telefoneEditado + " foi alterado para " + agendaTelefone[i] + " com sucesso!");
+                                        break;
+                                    case 3:
+                                        String emailEditado = agendaEmail[i];
+                                        System.out.println("Informe o email que deseja cadastrar: ");
+                                        agendaEmail[i] = input.nextLine();
+                                        System.out.println("O email " + emailEditado + " foi alterado para " + agendaEmail[i] + " com sucesso!");
+                                        break;
+                                    case 4:
+                                        System.out.println("Saindo....");
+                                        break;
+                                }
+                            }
+                            System.out.println(">>>>  Alteração finalizada <<<<");
+                            detalharContato(agendaId[i], agendaNome[i], agendaTelefone[i], agendaEmail[i]);
+                        }else {
+                            System.out.println("Não existe o contato com o telefone: " + telefone + " que deseja editar!");
+                        }
                     }
-                    break;
                 case 4:
-                    System.out.println("Informe o nome que deseja deletar: ");
-                    String deletar = input.nextLine();
-                    if (nomes.contains(deletar)) {
-                        nomes.remove(deletar);
-                        System.out.println("O nome " + deletar + " foi deletado com sucesso!");
-                    }else {
-                        System.out.println("O nome que deseja deletar não está na lista!");
-                    }
                     break;
                 case 5:
-                    System.out.println("LISTA DE NOMES VIGENTES");
-                    System.out.println(nomes);
+                    listarAgenda(agendaId, agendaNome, agendaTelefone, agendaEmail, agendaTelefone);
                 case 6:
                     System.out.println("Saindo....");
                     break;
@@ -72,4 +103,37 @@ public class agenda {
         }
 
     }
+
+    public static void menuAgenda() {
+        System.out.println(">>>>  Menu Contato <<<<");
+        System.out.println("1 - Adicionar Contato");
+        System.out.println("2 - Detalhar Contato");
+        System.out.println("3 - Editar Contato");
+        System.out.println("4 - Remover Contato");
+        System.out.println("5 - Listar Agenda");
+        System.out.println("6 - Sair");
+    }
+
+    public static void detalharContato(int id, String nome, String tel, String email) {
+        System.out.println(">>>>  Contato <<<<");
+        System.out.println("Id   |   Nome         |   Telefone      |   E-mail");
+        System.out.println(id + "   |   " + "   |   " + nome + "   |   " + tel + "   |   " + email);
+    }
+
+    public static void listarAgenda(int[] id, String[] nome, String[] tel, String[] email, String[] agenda) {
+        System.out.println(">>>>  Contato <<<<");
+        System.out.println("Id   |   Nome         |   Telefone      |   E-mail");
+        for (int i = 0; i < agenda.length; i++){
+            System.out.println(id[i] + "   |   " + "   |   " + nome[i] + "   |   " + tel[i] + "   |   " + email[i]);
+        }
+    }
+
+    public static void menuEdicao() {
+        System.out.println(">>>>  Editar Contato <<<<");
+        System.out.println("1 - Editar nome");
+        System.out.println("2 - Editar telefone");
+        System.out.println("3 - Editar e-mail");
+        System.out.println("4 - Sair");
+    }
+
 }
